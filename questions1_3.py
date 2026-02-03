056b9cd8df8baa653386428561f9d1609bc59a21
Mattcben1 <bxp6yd@virginia.edu>
2026-02-03 14:28:58 -0500

    Update comments with dataset review instructions
    Added instructions for reviewing datasets and formulating questions.

--- questions1_3.py
+++ questions1_3.py
@@ -1,1 +1,9 @@
-#Answer Questions Here
+# Answer Questions Here
+Step one: Review these two datasets and brainstorm problems that could be addressed with the dataset. Identify a question for each dataset.
+ 
+                                                                                                                         
+                                                                                                                         
+                                                                                                                         
+Write a generic question that this dataset could address.
+What is an independent Business Metric for your problem? 
+What do your instincts tell you about the data? Can it address your problem? What areas/items are you worried about?
